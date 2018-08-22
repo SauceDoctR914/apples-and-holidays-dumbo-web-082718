1a1b2de21a90977b9e62e11a931dd2722d4d5007
Gavin Soko <sokolofg@gmail.com>
2018-08-22 15:21:48 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,10 +18,13 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above
+ holiday_hash[:winter][:christmas] << supply 
+ holiday_hash[:winter][:new_years] << supply 
+ # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
 
@@ -29,23 +32,50 @@ end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
+  holiday_hash[:spring][:memorial_day] << supply 
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
 
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
+  holiday_hash.each do |seasons, holidays|
+    if season == holidays 
+      holiday_hash[seasons][holiday_name] = supply_array
+    end
+  end
+      
+  holiday_hash[season][holiday_name] = supply_array 
   # code here
   # remember to return the updated hash
+  holiday_hash
 
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
-
+  winterSupplies = []
+  holiday_hash.each do |holidays, supplies|
+    holiday_hash[:winter][holidays] = supplies.each do |element|
+      winterSupplies << element
+      winterSupplies.flatten!.uniq!
+    end
+  end
+  winterSupplies
 end
+      
+    # return an array of all of the supplies that are used in the winter season
+
 
 def all_supplies_in_holidays(holiday_hash)
+  holiday_hash.each do |season, holiday|
+    puts season.to_s.capitalize + ":"
+    binding.pry
+   holiday.each do |event, array|
+    puts holiday +": #{array.map do |index|
+       
+ end
+ end
+    
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
   #   Christmas: Lights, Wreath
